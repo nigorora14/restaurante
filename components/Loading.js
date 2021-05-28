e6530a51add78226b90ca20bf30c6351053bb7c9
nigorora14 <niltongr@outlook.com>
2021-05-28 18:14:12 -0500

    .

--- components/Loading.js
+++ components/Loading.js
@@ -1,12 +1,35 @@
 import React from 'react'
+import { ActivityIndicator } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
+import { Overlay } from 'react-native-elements/dist/overlay/Overlay'
 
 export default function Loading({isVisible, text}) {//va usar dos propiedades isVisible: activar y desactivar y text que va mostrar el login 
     return (
-        <View>
-            <Text></Text>
-        </View>
+        
+        <Overlay isVisible={isVisible}
+                 windowBackgoundColor="rgba(0,0,0,0.5)"
+                 overlayBackgroundColor="transparent"
+                 overlayStyle={styles.Overlay}
+        >
+         <View>
+             <ActivityIndicator>
+                 {
+                     text && <Text>{text}</Text>
+                 }
+             </ActivityIndicator>
+         </View>
+        </Overlay>
+
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    Overlay: {
+        height: 100,
+        width:200,
+        backgroundColor:"fff",
+        borderColor: "#442484",
+        borderWidth:2,
+        borderRadius: 10
+    }
+})
--- screens/account/UserGuest.js
+++ screens/account/UserGuest.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Loading from '../../components/Loading'
 
 export default function UserGuest() {
     return (
         <View>
             <Text>UserGuest</Text>
+            <Loading isVisible={true} text={"Cargando...."} />
         </View>
     )
 }
