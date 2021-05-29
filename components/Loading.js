b0ff6793942f51e1659e4ad753827e6eb0a23cee
nigorora14 <niltongr@outlook.com>
2021-05-28 22:12:50 -0500

    .

--- components/Loading.js
+++ components/Loading.js
@@ -1,35 +1,46 @@
 import React from 'react'
 import { ActivityIndicator } from 'react-native'
 import { StyleSheet, Text, View } from 'react-native'
-import { Overlay } from 'react-native-elements/dist/overlay/Overlay'
+import { Overlay } from 'react-native-elements/dist/overlay/Overlay' //
 
 export default function Loading({isVisible, text}) {//va usar dos propiedades isVisible: activar y desactivar y text que va mostrar el login 
     return (
         
         <Overlay isVisible={isVisible}
-                 windowBackgoundColor='rgba(0,0,0,0.5)'
+                 windowBackgoundColor="rgba(0,0,0,0.5)"
                  overlayBackgroundColor="transparent"
                  overlayStyle={styles.Overlay}
         >
-         <View>
-             <ActivityIndicator>
+         <View style={styles.view}>
+             <ActivityIndicator 
+                                size="large"
+                                color= "#442484"                   
+             />
                  {
-                     text && <Text>{text}</Text>
+                     text && <Text style={styles.text}>{text}</Text>
                  }
-             </ActivityIndicator>
          </View>
         </Overlay>
 
     )
 }
-
+//estilos
 const styles = StyleSheet.create({
     Overlay: {
-        height: 100,
-        width:200,
-        backgroundColor:"#fff",
-        borderColor: "#442484",
-        borderWidth:2,
-        borderRadius: 10
+                height: 100,
+                width:200,
+                backgroundColor:"#fff",
+                borderColor: "#442484",
+                borderWidth:2,
+                borderRadius: 10
+    },
+    view: {
+                flex: 1, //todo el ancho disponible
+                alignItems: "center", //los elementos esten centrados horizontalmente
+                justifyContent: "center" //centrar verticalmente
+    },
+    text: {
+                color: "#442484",
+                marginTop:1
     }
 })
--- screens/account/Account.js
+++ screens/account/Account.js
@@ -1,6 +1,8 @@
 //rnfs
 import React, {useState, useEffect} from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Loading from '../../components/Loading';
+import { getCurrentUser } from '../../utils/actions';
 
 import { firebaseApp } from '../../utils/firebase';
 import   firebase from 'firebase';
@@ -8,20 +10,21 @@ import   firebase from 'firebase';
 import UserGuest from './UserGuest';
 import UserLogged from './UserLogged';
 
+
+
 export default function Account() {
-    const [login,setLogin] = useState(null) //creando un estado donde el usuario no esta logeado
-    
-    
-        firebase.auth().onAuthStateChanged((user) => 
-        {
-            user !== null ? (setLogin(true)):setLogin(false) //indica que si esta logeado el usuario o no
-        })
-    
+    //creando un estado donde el usuario no esta logeado  
+    const [login,setLogin] = useState(null) 
+    //indica que si esta logeado el usuario o no
+    useEffect(() => {
+        const user = getCurrentUser()
+        user ? setLogin(true):setLogin(false)
+    }, [])
 
     //si el login es nulo
-    if (login==null) {
-        return <Text>Cargando...</Text>
-    }
+    //if (login==null) {
+        return <Loading isVisible={true} text = "Cargando..."/>
+    //}
     //si llega a este punto es por que termino la consulta del login
     return login ? <UserLogged/> : <UserGuest/>
 }
